bb30233aec0b7825e838e46e5c6dd8481a62168a
Elase Huang <elase.huang@outlook.com>
2023-01-12 10:51:20 +0800

    🧼refactor

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -5,11 +5,20 @@ async fn main() {
         let res = client.get("http://127.0.0.1:7878").send().await.unwrap();
         if res.status().is_success() {
             let body = res.text().await.unwrap();
-            println!("{}", body);
+            let redis_metrics = serde_json::from_str::<RedisMetrics>(body.as_str()).unwrap();
+            println!("{:?}", redis_metrics);
         } else {
             panic!("get request return error {}", res.text().await.unwrap())
         }
 
         std::thread::sleep(std::time::Duration::from_secs(5))
     }
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct RedisMetrics {
+    used_memory: u64,
+    total_memory: u64,
+    total_commands_processed: u64,
+    connected_clients: u64,
 }
