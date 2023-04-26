bd47db2fa04826768171b22cdb60098d94de0319
Elase Huang <elase.huang@outlook.com>
2023-04-26 14:57:15 +0800

    ✨feat: random 16 [A-Za-z0-9]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::{thread_rng, Rng};
+
+fn generate_random_string(length: usize) -> String {
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789";
+    let mut rng = thread_rng();
+    (0..length)
+        .map(|_| {
+            let index = rng.gen_range(0..CHARSET.len());
+            CHARSET[index] as char
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let random_string = generate_random_string(16);
+    println!("{}", random_string);
 }
