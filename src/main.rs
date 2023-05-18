36f7e7f991cc69c10c44f85c218a8cb39d055e51
Elase Huang <elase.huang@outlook.com>
2023-05-18 18:09:33 +0800

    🧼refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use bevy::core_pipeline::clear_color::ClearColorConfig;
 use bevy::prelude::*;
+use bevy::render::camera::RenderTarget;
+use bevy::render::render_resource::{
+    Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+};
+use bevy::render::texture::BevyDefault;
+use bevy::sprite::Material2dPlugin;
 
 mod debug;
 
@@ -6,6 +13,7 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_plugin(debug::DebugPlugin);
+    // app.add_plugin(Material2dPlugin::<PostProcessingMaterial>::default());
     app.add_startup_system(setup);
     app.run();
 }
@@ -13,15 +21,36 @@ fn main() {
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
+    windows: Query<&Window>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
 ) {
-    // camera
-    commands.spawn(Camera3dBundle {
-        projection: OrthographicProjection::default().into(),
-        transform: Transform::from_xyz(0.0, 0.0, 10.0).with_scale(Vec3::splat(0.01)),
+    let window = windows.single();
+
+    let size = Extent3d {
+        width: window.resolution.physical_width(),
+        height: window.resolution.physical_height(),
         ..default()
-    });
+    };
+
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::bevy_default(),
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+    image.resize(size);
+    let image_handle = images.add(image);
 
     // cubes
     commands.spawn(PbrBundle {
@@ -41,4 +70,31 @@ fn setup(
         transform: Transform::from_xyz(3.0, 8.0, 5.0),
         ..default()
     });
+
+    // // camera
+    // commands.spawn(Camera3dBundle {
+    //     projection: OrthographicProjection::default().into(),
+    //     transform: Transform::from_xyz(0.0, 0.0, 10.0).with_scale(Vec3::splat(0.01)),
+    //     ..default()
+    // });
+
+    // camera
+    commands.spawn((
+        Camera3dBundle {
+            camera_3d: Camera3d {
+                clear_color: ClearColorConfig::Custom(Color::BLACK),
+                ..default()
+            },
+            // camera: Camera {
+            //     target: RenderTarget::Image(image_handle.clone()),
+            //     ..default()
+            // },
+            projection: OrthographicProjection::default().into(),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0))
+                .looking_at(Vec3::ZERO, Vec3::Y)
+                .with_scale(Vec3::splat(0.01)),
+            ..default()
+        },
+        UiCameraConfig { show_ui: false },
+    ));
 }
