550fdef20e40dbf8ddc29c74946bfd9a7a13425a
Elase Huang <elase.huang@outlook.com>
2023-08-01 17:31:02 +0800

    🧼refactor: prevents additional console window on Windows in release

--- src/breakout.rs
+++ src/breakout.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 //! A simplified implementation of the classic game "Breakout".
 //! => Original example by Bevy, modified for Bevy Quinnet to add a 2 players versus mode.
 
