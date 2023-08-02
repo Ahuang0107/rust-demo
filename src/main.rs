6126c95f7c79ea36c526d2bd081c3edd79ab42ea
Elase Huang <elase.huang@outlook.com>
2023-08-02 10:23:33 +0800

    🧼refactor: setup camera

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,11 @@ fn main() {
 
     app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
 
+    app.add_systems(Startup, setup_camera);
+
     app.run();
+}
+
+fn setup_camera(mut c: Commands) {
+    c.spawn(Camera2dBundle::default());
 }
