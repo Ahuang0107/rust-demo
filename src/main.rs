b2e6171471835f3155b9058967234dbcfabfe0f6
elase huang <elase.huang@outlook.com>
2023-08-27 04:32:42 +0800

    🧼refactor: headless_chrome 的最基础用法

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use headless_chrome::{Browser, LaunchOptions};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(false)
+            .build()
+            .expect("Could not find chrome-executable"),
+    )?;
+    let tab = browser.new_tab()?;
+    tab.navigate_to("https://wikipedia.org")?
+        .wait_for_element("input#searchInput")?;
+    tab.type_str("Elvis Aaron Presley")?;
+    tab.wait_for_element("button[type='submit']")?.click()?;
+    Ok(())
 }
