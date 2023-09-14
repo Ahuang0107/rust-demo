b613261b69e1e0e4c204ce71875bcd723198792d
elase huang <elase.huang@outlook.com>
2023-09-14 08:49:16 +0800

    🧼refactor

---
+++ assets/shaders/custom_material.wgsl
@@ -0,0 +1,26 @@
+#import bevy_sprite::mesh2d_vertex_output MeshVertexOutput
+
+struct CustomMaterial {
+    color: vec4<f32>,
+};
+
+@group(1) @binding(0)
+var<uniform> material: CustomMaterial;
+@group(1) @binding(1)
+var base_texture: texture_2d<f32>;
+@group(1) @binding(2)
+var base_sampler: sampler;
+@group(1) @binding(3)
+var mask_texture: texture_2d<f32>;
+@group(1) @binding(4)
+var maks_sampler: sampler;
+
+@fragment
+fn fragment(
+    mesh: MeshVertexOutput,
+) -> @location(0) vec4<f32> {
+    var base = textureSample(base_texture, base_sampler, mesh.uv);
+    var mask = textureSample(mask_texture, maks_sampler, mesh.uv);
+    return base;
+//    return vec4<f32>(base.xyz, (1.0 - mask.w) * base.w);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,57 @@
 use bevy::prelude::*;
+use bevy::reflect::{TypePath, TypeUuid};
+use bevy::render::render_resource::{AsBindGroup, ShaderRef};
+use bevy::sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     let mut app = App::new();
 
     app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
+    app.add_plugins(WorldInspectorPlugin::new());
+    app.add_plugins(Material2dPlugin::<CustomMaterial>::default());
 
     app.add_systems(Startup, setup_camera);
 
     app.run();
 }
 
-fn setup_camera(mut c: Commands) {
+fn setup_camera(
+    mut c: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
     c.spawn(Camera2dBundle::default());
+    c.spawn(MaterialMesh2dBundle {
+        mesh: meshes
+            .add(Mesh::from(shape::Quad::new(Vec2::new(161.0, 94.0))))
+            .into(),
+        material: materials.add(CustomMaterial {
+            color: Color::RED,
+            base_texture: asset_server.load("drawer copy.png"),
+            mask_texture: asset_server.load("mask copy.png"),
+        }),
+        // transform: Transform::from_scale(Vec3::splat(500.0)),
+        ..Default::default()
+    });
+}
+
+impl Material2d for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/custom_material.wgsl".into()
+    }
+}
+
+#[derive(AsBindGroup, TypeUuid, TypePath, Debug, Clone)]
+#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
+pub struct CustomMaterial {
+    #[uniform(0)]
+    color: Color,
+    #[texture(1)]
+    #[sampler(2)]
+    base_texture: Handle<Image>,
+    #[texture(3)]
+    #[sampler(4)]
+    mask_texture: Handle<Image>,
 }
