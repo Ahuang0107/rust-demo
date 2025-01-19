7de847308868eaae83a86e0a9d0bfd7bc5f0e141
Elase Huang <elase.huang@outlook.com>
2025-01-19 22:35:04 +0800

    feat: bevy update to 0.15.1

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use bevy::prelude::*;
 
+#[cfg(feature = "debug")]
+mod debug;
+
 fn main() {
     let mut app = App::new();
 
     app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
-
     app.add_systems(Startup, setup_camera);
+
+    #[cfg(feature = "debug")]
+    app.add_plugins(debug::DebugPlugin);
 
     app.run();
 }
 
-fn setup_camera(mut c: Commands) {
-    c.spawn(Camera2dBundle::default());
+fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("icon.png"),
+            ..default()
+        },
+        Transform {
+            scale: Vec3::splat(5.0),
+            ..default()
+        },
+    ));
 }
